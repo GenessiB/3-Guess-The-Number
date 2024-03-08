742cc0a7fd0a53e3686a85f0763b33a4a479af31
Genessi Barot <genessibarot@gmail.com>
2024-03-08 11:38:10 -0500

    working on instrusction popup

--- guess-the-number/JS/index.js
+++ guess-the-number/JS/index.js
@@ -2,12 +2,32 @@ const yesBtnElement = document.querySelector(".yes-btn");
 const landingPopupElement = document.querySelector(".play");
 const gameMenuElement = document.querySelector(".game-menu");
 
+const normalGamePopupElement = document.querySelector(".normal-game");
+const normalGameBtnElement = document.querySelector(".normal-btn");
+
+const reverseGamePopupElement = document.querySelector(".reverse-game");
+const reverseGameBtnElement = document.querySelector(".reverse-btn");
+
+const instructionsBtnElement = document.querySelector(".instructions");
+const instructionsPopupElement = document.querySelector(".instructions-popup");
+const closeSealBtnElement = document.querySelector(".close-seal");
 
 // Events
 function readyToPlay() {
-    landingPopupElement.classList.replace("visible", "hidden");
-    gameMenuElement.classList.replace("hidden", "visible");
-    }
+  landingPopupElement.classList.replace("visible", "hidden");
+  gameMenuElement.classList.replace("hidden", "visible");
+}
+// opens and closes the instructions/hint
+function seeInstructions() {
+  gameMenuElement.classList.replace("visible", "hidden");
+  instructionsPopupElement.classList.replace("hidden", "visible");
+}
+function closeInstructions() {
+  instructionsPopupElement.classList.replace("visible", "hidden");
+  gameMenuElement.classList.replace("hidden", "visible");
+}
 
 // Event Listeners
 yesBtnElement.addEventListener("click", readyToPlay);
+instructionsBtnElement.addEventListener("click", seeInstructions);
+closeSealBtnElement.addEventListener("click", closeInstructions);
